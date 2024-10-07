a4e94409f662c406fe0d4d94b29209844f51ea65
Julio <youremail@example.com>
2024-10-07 11:36:42 -0400

    GLAB340.2.1

---
+++ ArithmeticOperators.py
@@ -0,0 +1,13 @@
+a = 32
+b = 6 
+print('Addition of two numers: ', a + b)
+print('Subtraction of two numers: ', a - b)
+print('Multiplication of two numers: ', a * b)
+print('Division of two numers: ', a / b)
+print('Reminder of two numers: ', a % b)
+print('Exponnents of two numers: ', a ** b)
+print('Floor division of two numers: ', a // b)
+
+MultipleOperators = 1 + 2 * 3 / 4.0
+
+print("Result of MultipleOperators: ", MultipleOperators)
---
+++ AssignmentOperators.py
@@ -0,0 +1,10 @@
+a = 32
+b = 6
+
+print('a=b:', a==b) #
+print('a+=b:', a+b) # SH: a = a + b
+print('a-=b:', a-b) # SH: a = a - b
+print('a*=b:', a*b) # SH: a = a * b
+print('a%=b:', a%b) # SH: a = a % b
+print('a**=b:', a**b) # SH: a = a ** b 
+print('a//=b:', a//b) # SH: a = a / b
